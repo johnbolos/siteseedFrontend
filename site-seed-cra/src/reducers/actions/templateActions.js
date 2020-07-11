e1b5ca228522a22989e60c8ddf44e66b19b38814
protomac <mayankyadav891999@gmail.com>
2020-07-12 01:10:28 +0530

    duplicate folders/files delete

--- site-seed-cra/src/reducers/actions/templateActions.js
+++
@@ -1,7 +0,0 @@
-export const selectTemplate = templateName => dispatch => {
-  console.log("action dispatched", templateName)
-  dispatch({
-    type: "CHANGE_TEMPLATE",
-    payload: templateName,
-  })
-}
